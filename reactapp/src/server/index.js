4bc226f0a73cb5e8d15a8b459a434c7d9643e77f
Samyukta Iyer <51866832+samyukta-iyer@users.noreply.github.com>
2023-01-29 14:00:50 -0500

    windows client mesg comment

--- reactapp/src/server/index.js
+++ reactapp/src/server/index.js
@@ -9,7 +9,12 @@ app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().use
 // app.get('/api/test', (req, res) => fetch('../client/estimatedTime.json').then((json) => res.send(json)));
 app.get('/api/test2', (req, res) => {
     console.log(__dirname)
-    const file = fs.readFileSync(__dirname + "\\\..\\\client\\\\estimatedTime.json");
+    const file = fs.readFileSync(__dirname + "/../client/estimatedTime.json");
+    // for windows, use below inst of line 12: 
+    // const file = fs.readFileSync(__dirname + "\\\..\\\client\\\estimatedTime.json");
+    
+    
+    
     console.log(JSON.parse(file));
     res.send(JSON.parse(file));
 });
